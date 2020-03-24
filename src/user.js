61724641d77a9d81d1eda10adc015bcccd2586e0
Olivia Webster <oliviacweb@gmail.com>
2020-03-24 17:50:26 -0600

    Pass user tests of properties

--- src/user.js
+++ src/user.js
@@ -1,11 +1,10 @@
 class User {
   constructor(id, name, pantry){
-    // this.id = id;
-    // this.name = name;
-    // this.pantry = pantry;
-    // this.favoriteRecipes = [];
-    //
-
+    this.id = id;
+    this.name = name;
+    this.pantry = pantry;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
   }
 
 
