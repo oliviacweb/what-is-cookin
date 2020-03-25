2cc3dbf98f5407aa19e0b186c468cb1b63f5a418
Olivia Webster <oliviacweb@gmail.com>
2020-03-25 17:11:09 -0600

    Display greeting of random user first name on page load

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+let displayWelcome = document.querySelector('.welcome-header');
+let randomUser;
+window.onload = function() {
+  generateUser();
+  greetUser();
+}
+
+function generateUser(userData) {
+  randomIndex = returnRandomNumber();
+  randomUser = usersData[randomIndex];
+  user = new User(randomUser.id, randomUser.name, randomUser.pantry);
+}
+
+function returnRandomNumber() {
+  randomNumber = Math.floor(Math.random() * (50 - 1) + 1);
+  return randomNumber;
+}
+
+function greetUser() {
+  displayWelcome.innerText = `Welcome, ${returnFirstName()}!`;
+}
+
+function returnFirstName() {
+  var firstName = user.name.split(" ");
+  return firstName[0];
+}
