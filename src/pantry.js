bfd0a3e8b82fd6386c2d8b03a6de6afc93dcf6ba
Olivia Webster <oliviacweb@gmail.com>
2020-04-02 05:06:07 -0400

    Lint pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -4,7 +4,7 @@ class Pantry {
     this.recipe = recipe;
     this.thingsToBuy = [];
     this.amountToBuy = [];
-  };
+  }
 
   evaluateIngredientsForRecipes() {
     console.log(this.recipe);
@@ -21,20 +21,20 @@ class Pantry {
     requiredIngredients.forEach(ingredient => {
       const found = pantryIngredients.find(item => item.ingredient === ingredient.id);
 
-      if(found && found.amount >= ingredient.amount){
+      if (found && found.amount >= ingredient.amount){
         iHave.push(found);
         return `You have enough of this ingredient`;
-      } else if (found && found.amount < ingredient.amount){
+      } else if (found && found.amount < ingredient.amount) {
         this.amountToBuy.push(found);
         return `You have this ingredient but not enough`;
-      };
+      }
 
-      if(!found) {
+      if (!found) {
         this.thingsToBuy.push(ingredient);
         return `You need this ingredient`;
       }
     });
-  };
+  }
 
   findIngredientsNeeded() {
     const recipeIngredients = this.recipe.ingredients;
@@ -47,14 +47,14 @@ class Pantry {
     const amountToBuy = this.amountToBuy;
     const ingredientsNeeded = [];
 
-    if(this.thingsToBuy.length > 0) {
+    if (this.thingsToBuy.length > 0) {
       this.thingsToBuy.forEach(item => ingredientsNeeded.push(item))
-    };
+    }
 
-    if(this.amountToBuy.length > 0) {
+    if (this.amountToBuy.length > 0) {
       requiredIngredients.forEach(recipeIngredient => {
         amountToBuy.forEach(item => {
-          if(recipeIngredient.id === item.ingredient){
+          if (recipeIngredient.id === item.ingredient) {
             const newIngObj = recipeIngredient;
             const needed = recipeIngredient.amount - item.amount;
             newIngObj.amount = needed;
@@ -62,13 +62,10 @@ class Pantry {
           }
         })
       })
-    };
+    }
     return ingredientsNeeded;
-  };
+  }
 
-  removeIngredients() {
-
-  };
 };
 
 if (typeof module !== 'undefined') {
